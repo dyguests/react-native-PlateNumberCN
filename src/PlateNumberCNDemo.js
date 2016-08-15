95eb12c5ee350233d53954f369b0f8fbb0532bd0
fanhl <dyguests@qq.com>
2016-08-15 17:59:19 +0800

    重写入口页面

--- src/PlateNumberCNDemo.js
+++ src/PlateNumberCNDemo.js
@@ -14,53 +14,28 @@ import PlatNumPicker from './component/lin_platNumPicker/PlatNumPicker';
 
 export default class PlateNumberCNDemo extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {modalVisible: false};
-    }
-
-    setModalVisible(visible) {
-        this.setState({modalVisible: visible});
-    }
 
     render() {
         return (
             <View style={{marginTop: 22}}>
-                <Modal
-                    animationType={"slide"}
-                    transparent={false}
-                    visible={this.state.modalVisible}
-                    onRequestClose={() => {
-                        alert("Modal has been closed.")
-                    }}
-                >
-                    <View style={{marginTop: 22}}>
-                        <View>
-                            <Text>Hello World!</Text>
-
-                            <TouchableHighlight onPress={() => {
-                                this.setModalVisible(!this.state.modalVisible)
-                            }}>
-                                <Text>Hide Modal</Text>
-                            </TouchableHighlight>
-
-                        </View>
-                    </View>
-                </Modal>
-
                 <TouchableHighlight onPress={() => {
-                    this.setModalVisible(true)
+                    this.showPlatNumPicker(true)
                 }}>
-                    <Text>Show Modal</Text>
+                    <Text>Show PlateNumPicker</Text>
                 </TouchableHighlight>
                 <PlatNumPicker
                     onPlateNumberSelected={(city, letter)=> {
-                        {/*alert(`${city}, ${letter}`);*/}
+                        {/*alert(`${city}, ${letter}`);*/
+                        }
                     }}
                 />
             </View>
         );
     }
+
+    showPlatNumPicker = ()=> {
+
+    };
 }
 
 const styles = StyleSheet.create({
